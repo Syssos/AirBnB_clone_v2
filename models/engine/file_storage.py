9a077851a22a34ac5559983a2c212d603eacf574
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-23 21:05:41 +0000

    Adding delete method to github

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         '''
             Return the dictionary
         '''
@@ -53,3 +53,14 @@ class FileStorage:
                 FileStorage.__objects[key] = class_name(**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        '''
+            Deleting an object from __objects
+        '''
+        copy = dict(FileStorage.__objects)
+
+        for key, val in copy.items():
+            if val == obj:
+                del(FileStorage.__objects[key])
+            self.save()
