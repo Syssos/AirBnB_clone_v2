bff7d9f361a10b76d49b2e87a9ab0b72ea6a2ac5
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-24 23:39:44 +0000

    Adding updated state for pep8

--- models/state.py
+++ models/state.py
@@ -13,8 +13,7 @@ class State(BaseModel):
     __tablename__ = "states"
 
     name = Column((String(128)), nullable=False)
-    cities = relationship("City", backref="state", cascade="all,
-                          delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     @property
     def cities(self):
