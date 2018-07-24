35bad408bb588656681e9c9e0c4eb2be142873f5
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-24 21:42:48 +0000

    Adding updated state with getter grabbing values for city and giving reference to objects

--- models/state.py
+++ models/state.py
@@ -10,4 +10,20 @@ class State(BaseModel):
     '''
         Implementation for the State.
     '''
-    name = ""
+    __tablename__ = "states"
+
+    name = Column((String(128)), nullable=False)
+    cities = relationship("City", backref="state", cascade="all,
+                          delete-orphan")
+
+    @property
+    def cities(self):
+        '''
+            This script will get all the City values
+
+        '''
+        listofcities = models.storage.all("City").values()
+
+        for city in listofcities:
+            if city.state_id == self.id:
+                return city
