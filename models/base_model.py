eda5bf2981c97bf1230879266d5acc7a58305699
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-24 18:09:55 +0000

    Adding Datetime for sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@
 import uuid
 from datetime import datetime
 import models
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
