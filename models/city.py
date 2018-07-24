ec6da3929a0b6e0194eeafe4ed91bfb0bd5aab32
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-24 18:06:18 +0000

    Adding updated content for city

--- models/city.py
+++ models/city.py
@@ -3,11 +3,15 @@
     Define the class City.
 '''
 from models.base_model import BaseModel
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column((String(128)), nullable=False)
+    state_id = Column(String(60)), nullable=False, ForeignKey("states.id"))
