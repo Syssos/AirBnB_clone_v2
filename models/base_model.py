4d5196e48258bfd4ce0dfaa4dbee220bdd3a1795
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-24 18:07:29 +0000

    Adding updated information, needing to alter init

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,19 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     '''
         Base class for other classes to be used for the duration.
     '''
+    id = Column(String(60)), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nnullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         '''
             Initialize public instance attributes.
@@ -19,7 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
@@ -29,6 +36,7 @@ class BaseModel:
                 if "__class__" not in key:
                     setattr(self, key, val)
 
+                else:
     def __str__(self):
         '''
             Return string representation of BaseModel class
@@ -48,6 +56,7 @@ class BaseModel:
             Update the updated_at attribute with new.
         '''
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -59,4 +68,14 @@ class BaseModel:
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
+        if "_sa_instance_state" in cp_dct:
+            del cp_dct["_sa_instance_state"]
+
         return (cp_dct)
+
+
+    def delete(self):
+        '''
+           This method will delete a current instance of a storage
+        '''
+        models.storage.delete(self)
