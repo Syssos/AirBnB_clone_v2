2a021e1c19b5a379dd90047db7777b8542107f50
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-25 07:43:54 +0000

    Adding current content of city

--- models/city.py
+++ models/city.py
@@ -2,16 +2,16 @@
 '''
     Define the class City.
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
     __tablename__ = "cities"
-    name = Column((String(128)), nullable=False)
+    name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
