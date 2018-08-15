b0083c2dc534dc6b3f9f939197a3d72643ec35fc
Syssos <Syssos@gitgub.com>
2018-08-15 12:10:48 -0700

    display hbnb on /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """ Displays Hello Holberton
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Displays HBNB on "/hbnb"
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
